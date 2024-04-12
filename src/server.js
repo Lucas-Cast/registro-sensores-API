58dd3224716b5fef59fa1eaa0ea83469ca92108d
Lucas Castelano <100870230+Lucas-Cast@users.noreply.github.com>
2024-04-11 23:24:43 -0300

    Criação de rotas no server.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
 const port = 3004
 
 const express = require('express')
-const app = express()
+const serverApp = express()
+const routes = require('./controllers/measureControllers.js');
+
+serverApp.use(express.json());
+serverApp.use(routes);
 
 const db = require('./db.js')
 
 db.connect()
-    .then(() => app.listen(port))
+    .then(() => serverApp.listen(port))
     .catch(err =>console.log(err))
-    
-
- 
 
 
